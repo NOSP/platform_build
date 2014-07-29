93a11a01687b25cda5c84e909e761fd96d025ed2
XsMagical <XsMagical@team-nocturnal.net>
2014-07-29 18:01:01 -0400

    Update main.mk

--- core/main.mk
+++ core/main.mk
@@ -144,8 +144,8 @@ javac_version_str := $(shell unset _JAVA_OPTIONS && javac -version 2>&1)
 ifeq ($(LEGACY_USE_JAVA6),)
 required_version := "1.6.x"
 required_javac_version := "1.6"
-java_version := $(shell echo '$(java_version_str)' | grep '^java .*[ "]1\.7[\. "$$]')
-javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.7[\. "$$]')
+java_version := $(shell echo '$(java_version_str)' | grep '^java .*[ "]1\.6[\. "$$]')
+javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.6[\. "$$]')
 else # if LEGACY_USE_JAVA6
 required_version := "1.6.x"
 required_javac_version := "1.6"
