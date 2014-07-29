36893a8d13e662245ee6f8bbeca982f376374c69
XsMagical <XsMagical@team-nocturnal.net>
2014-07-29 17:59:33 -0400

    Update main.mk

--- core/main.mk
+++ core/main.mk
@@ -142,8 +142,8 @@ javac_version_str := $(shell unset _JAVA_OPTIONS && javac -version 2>&1)
 # Check for the correct version of java, should be 1.7 by
 # default, and 1.6 if LEGACY_USE_JAVA6 is set.
 ifeq ($(LEGACY_USE_JAVA6),)
-required_version := "1.7.x"
-required_javac_version := "1.7"
+required_version := "1.6.x"
+required_javac_version := "1.6"
 java_version := $(shell echo '$(java_version_str)' | grep '^java .*[ "]1\.7[\. "$$]')
 javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.7[\. "$$]')
 else # if LEGACY_USE_JAVA6
